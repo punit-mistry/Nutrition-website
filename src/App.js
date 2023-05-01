30790957319dff341abf71b32259ad0f796539c9
Punit <Punitmistr@gmail.com>
2023-05-01 19:04:47 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Router ,Routes,Route, Link} from 'react-router-dom'
+import Nutrition from './Nutrition'
+const App = () => {
+  return (
+<>
+
+           <div className="Navbar">  
+          <div className='Logo'>
+            <img src='https://templatekit.jegtheme.com/nouriv/wp-content/uploads/sites/365/2023/04/logo-nouriv.png'  />
+          </div>
+          <div className='Content'><div>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/'>About</Link></li>
+              <li><Link to='/'>Page</Link></li>
+              <li><Link to='/'>Blog</Link></li>
+            </ul>
+            </div></div>
+          <div className='Button'>
+            <div>
+            <button >Contact Us</button>
+            </div>
+            </div>
+
+          </div>  
+          <Routes>
+            <Route path='/' element={<Nutrition />}></Route>
+          </Routes>
+   
+    </>
+  )
+}
+
+export default App
