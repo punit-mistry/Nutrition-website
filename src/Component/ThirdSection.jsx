3e7f2a61176d6a192d77f320e44d6cf7822c6b84
Punit <Punitmistr@gmail.com>
2023-05-03 23:00:20 +0530

    Added the New List Api in the Third Section

--- src/Component/ThirdSection.jsx
+++ src/Component/ThirdSection.jsx
@@ -3,7 +3,9 @@ import axios from "axios";
 const ThirdSection = () => {
 const [food,setFood] =useState([])
 const [searchFood,setsearchFood] =useState([])
+const [List,setList] =useState([])
 const [search, setSearch] = useState('');
+const [newfood, setnewFood] = useState([]);
 
   function handleChange(event) {
     event.preventDefault();
@@ -31,6 +33,7 @@ const [search, setSearch] = useState('');
   }
 useEffect(() => {
 let respone=[];
+let list=[];
   let config = {
     method: 'get',
     url: 'https://www.themealdb.com/api/json/v1/1/filter.php?i=',
@@ -48,9 +51,26 @@ let respone=[];
   .catch((error) => {
     console.log(error);
   });
+  let config_list = {
+    method: 'get',
+    url: 'https://www.themealdb.com/api/json/v1/1/list.php?c=list',
+  };
+  
+  axios.request(config_list)
+  .then((response) => {
+    for(var i=0; i<10; i++) {
+      list.push(response.data.meals[i]);
+    
+    }
+    console.log(list,"this is the category list ")
+    setList(list)
+  })
+  .catch((error) => {
+    console.log(error);
+  });
   
   
-},[0]);
+},[]);
 
   return (
    <>
@@ -63,11 +83,52 @@ let respone=[];
   return(
       <div className='First' style={{margin:20,}}>
     <div ><img src={res.strMealThumb} width={350}/></div>
-    <div className='text'>{res.strMeal}</div>
+    <div className='text'>{res.strMeal}
+  
+    </div>
       </div>
   )
 })}
 </div>
+
+<div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+{List.map((res)=>{
+  return(
+<div className='List'>
+<div className='List_div'>
+  {res.strCategory}
+
+</div>
+
+  </div>
+
+  )
+})
+}
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <div className='searchbox'>
       <form onSubmit={handleChange}>
@@ -89,7 +150,9 @@ let respone=[];
         <div ><img src={res?.strMealThumb || null} width={350}/></div>
         <div className='text'>{res?.strMeal || null}</div>
         <br/>
-        <div><button onClick={()=>{console.log(res.idMeal)}}>Click Here To Know More</button></div>
+        <div >
+          <p style={{display:"none"}}>{res?.idmeal}</p>
+          <button onClick={()=>{ return setnewFood([]),setnewFood([...newfood, res]);}}>Click Here To Know More</button></div>
           </div>
          
         )
@@ -98,6 +161,63 @@ let respone=[];
      </div>
      
     </div>
+
+
+
+{
+  newfood&&
+  newfood.map((res)=>{
+  return(
+    <>
+      <table >
+        <tbody>
+                  <tr>
+                    <th>Image</th>
+                    <td><img src={res.strMealThumb} width={150}/></td>
+                  </tr>
+                  <tr>
+                    <th>Name</th>
+                    <td>{res.strMeal}</td>
+                  </tr>
+                  <tr>
+                    <th>Area</th>
+                    <td>{res.strArea}</td>
+                  </tr>
+                  <tr>
+                    <th>Category</th>
+                    <td>{res.strCategory}</td>
+                  </tr>
+                  <tr>
+                    <th>Ingredient</th>
+                    <td>{res.strIngredient1},{res.strIngredient2},{res.strIngredient3},{res.strIngredient4},{res.strIngredient5},{res.strIngredient6},{res.strIngredient7},{res.strIngredient8},{res.strIngredient9}</td>
+                  </tr>
+                  <tr>
+                    <th>Instructions</th>
+                    <td>{res.strInstructions}</td>
+                  </tr>
+                  <tr>
+                    <th>Watch Here </th>
+                    <td><a href={res.strYoutube} target='_blank'>CLick Here</a></td>
+                  </tr>
+                </tbody>
+      </table>
+    </>
+  )
+})
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 
    </>
   )
