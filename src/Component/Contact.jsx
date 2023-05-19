451e77d6c8615997e30510126ee1f3b54c052b33
Punit <Punitmistr@gmail.com>
2023-05-19 20:24:44 +0530

    Post Api Contact form

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import Image from '../Images/image2.webp'
+import React,{useState} from 'react'
+import Image from '../Images/contact-image.png'
 import Spline from '@splinetool/react-spline';
 import { motion } from 'framer-motion';
+import axios from 'axios';
 
 const Contact = () => {
+const [userdata,setUsetData]   =useState({
+    name:"",
+    number:"",
+    email:"",
+    message:"",
+})
+
+    const postuserdata =(e)=>{
+     
+        const name = e.target.name
+        const value = e.target.value
+        setUsetData({...userdata,[name]:value});
+        console.log(userdata)
+    }
+
+    const submitData=async(e)=>{
+        e.preventDefault()
+        let data = userdata
+          let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://contact-form-data-4a590-default-rtdb.firebaseio.com/userdatabase.json',
+            headers: { 
+              'Content-Type': 'application/json'
+            },
+            data : data
+          };
+          
+          axios.request(config)
+          .then((response) => {
+            console.log(response.data)
+            alert("Thank you !!")
+          })
+          .catch((error) => {
+            alert("Oops Something went wrong!!")
+            console.log(error);
+          });
+          
+    }
+
   return (
     <motion.div 
         initial={{width:0}}
@@ -22,28 +63,32 @@ const Contact = () => {
     <Spline scene="https://prod.spline.design/flnHw-qOqMAWzB3p/scene.splinecode" />
         </div>  
         <div className='contact-form'>
-            <h1> Contact Form </h1>
+            
+<div className='form-context'>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac
+            rhoncus quam.
+            <br /><br />
+<img src={Image} />
 
-            <form>
-                <label>
-                   Full Name:
-                <input type='text' className='contact'/>
-                </label>
-                <label>
-                    Phone Number:
-                <input type='number' className='contact'/>
-                </label>
-                <label>
-                    Email ID:
-                <input type='text' className='contact'/>
-                </label>
-                <label>
-                    Message:<br/>
-                <textarea cols={72} rows={5}></textarea>
-                </label>
+</div>
+<div className='form'>
+<form>
+                
+                <input placeholder='Name' value={userdata.name}onChange={postuserdata} name="name" type='text' className='contact'/>
+               
                 <br/>
-                <button >Send !!</button>
+                <input placeholder='Number'value={userdata.number}onChange={postuserdata}name='number' type='number' className='contact'/>
+                <br/>
+                <input placeholder='Email'value={userdata.email}onChange={postuserdata} name='email'type='text' className='contact'/>
+                <br/>
+                <textarea placeholder='Message'value={userdata.message}onChange={postuserdata}name='message' cols={57} rows={5}></textarea>
+              
+                <br/>      <br/>
+                <button onClick={submitData}>Send !!</button>
             </form>
+
+</div>
+           
         </div>
 
     </motion.div>
